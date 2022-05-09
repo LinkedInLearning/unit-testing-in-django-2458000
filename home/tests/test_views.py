b8781bb3f4c0b287f3becf40978266d3eee580bf
Leticia Portella <leportella@gmail.com>
2022-05-09 22:32:21 +0100

    recorded

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -2,18 +2,22 @@ import pytest
 
 from django.contrib.auth.models import User
 
-def test_home_view(client):
+def test_home_endpoint_returns_a_welcome_page(client):
     response = client.get(path='/')
     assert response.status_code == 200
     assert 'Welcome to SmartNotes!' in str(response.content)
 
-def test_signup_get(client):
+def test_signup_endpoint_returns_from_for_unauthenticated_user(client):
     response = client.get(path='/signup')
     assert response.status_code == 200
     assert 'Enter the same password as before, for verification' in str(response.content)
 
 @pytest.mark.django_db
-def test_signup_authenticated(client):
+def test_signup_endpoint_redirects_authenticated_user(client):
+    '''
+      When a user is authenticated and try to access the 
+      signup page, they are redirected to the list of their notes.
+    '''
     user = User.objects.create_user('Clara', 'clara@test.com', 'password')
     client.login(username=user.username, password='password')
     assert user.is_authenticated
