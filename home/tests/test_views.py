a3613ac72e8b8ab24f38c8a2c674260556d278b7
Leticia Portella <leportella@gmail.com>
2022-05-09 21:32:24 +0100

    recorded

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -1,4 +1,24 @@
+import pytest
+
+from django.contrib.auth.models import User
+
 def test_home_view(client):
     response = client.get(path='/')
     assert response.status_code == 200
     assert 'Welcome to SmartNotes!' in str(response.content)
+
+def test_signup_get(client):
+    response = client.get(path='/signup')
+    assert response.status_code == 200
+    assert 'Enter the same password as before, for verification' in str(response.content)
+
+@pytest.mark.django_db
+def test_signup_authenticated(client):
+    user = User.objects.create_user('Clara', 'clara@test.com', 'password')
+    client.login(username=user.username, password='password')
+    assert user.is_authenticated
+
+    response = client.get(path='/signup', follow=True)
+    print(type(response))
+    assert response.status_code == 200
+    assert 'notes/notes_list.html' in response.template_name
