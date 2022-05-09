55a7efca32b82285fe153c01a457414e630b9df2
Leticia Portella <leportella@gmail.com>
2022-05-09 22:58:36 +0100

    recorded

--- notes/tests/test_notes_views.py
+++ notes/tests/test_notes_views.py
@@ -21,3 +21,22 @@ def test_list_endpoint_returns_user_notes(client):
     assert "Another note" in content
     assert 2 == content.count('<h3>')
 
+@pytest.mark.django_db
+def test_list_endpoint_only_returns_notes_from_authenticated_user(client):
+    jorge = User.objects.create_user('Jorge', 'jorge@test.com', 'password')
+    Notes.objects.create(title="A particular title", text="some text", user=jorge)
+
+    user = User.objects.create_user('Clara', 'clara@test.com', 'password')
+    client.login(username=user.username, password='password')
+
+    note = Notes.objects.create(title="a title", text="some text", user=user)
+    second_note = Notes.objects.create(title="Another note", text="some text", user=user)
+
+    response = client.get('/smart/notes')
+    content = str(response.content)
+
+    assert 200 == response.status_code
+    assert "a title" in content
+    assert "Another note" in content
+    assert "A particular title" not in content
+    assert 2 == content.count('<h3>')
