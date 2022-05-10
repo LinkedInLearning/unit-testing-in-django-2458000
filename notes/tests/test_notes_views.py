b0586fb7a87f5b9e9e288078ff3484abdf3a0229
Leticia Portella <leportella@gmail.com>
2022-05-10 11:42:37 +0100

    recorded

--- notes/tests/test_notes_views.py
+++ notes/tests/test_notes_views.py
@@ -4,14 +4,16 @@ from django.contrib.auth.models import User
 
 from notes.models import Notes
 
+@pytest.fixture
+def logged_user(client):
+    user = User.objects.create_user('Clara', 'clara@test.com', 'password')
+    client.login(username=user.username, password='password') 
+    return user
 
 @pytest.mark.django_db
-def test_list_endpoint_returns_user_notes(client):
-    user = User.objects.create_user('Clara', 'clara@test.com', 'password')
-    client.login(username=user.username, password='password')
-
-    note = Notes.objects.create(title="a title", text="some text", user=user)
-    second_note = Notes.objects.create(title="Another note", text="some text", user=user)
+def test_list_endpoint_returns_user_notes(client, logged_user):
+    note = Notes.objects.create(title="a title", text="some text", user=logged_user)
+    second_note = Notes.objects.create(title="Another note", text="some text", user=logged_user)
 
     response = client.get('/smart/notes')
     content = str(response.content)
@@ -22,15 +24,12 @@ def test_list_endpoint_returns_user_notes(client):
     assert 2 == content.count('<h3>')
 
 @pytest.mark.django_db
-def test_list_endpoint_only_returns_notes_from_authenticated_user(client):
+def test_list_endpoint_only_returns_notes_from_authenticated_user(client, logged_user):
     jorge = User.objects.create_user('Jorge', 'jorge@test.com', 'password')
     Notes.objects.create(title="A particular title", text="some text", user=jorge)
 
-    user = User.objects.create_user('Clara', 'clara@test.com', 'password')
-    client.login(username=user.username, password='password')
-
-    note = Notes.objects.create(title="a title", text="some text", user=user)
-    second_note = Notes.objects.create(title="Another note", text="some text", user=user)
+    note = Notes.objects.create(title="a title", text="some text", user=logged_user)
+    second_note = Notes.objects.create(title="Another note", text="some text", user=logged_user)
 
     response = client.get('/smart/notes')
     content = str(response.content)
